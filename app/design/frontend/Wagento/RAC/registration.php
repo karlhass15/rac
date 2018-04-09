e37d30da3c5d929be16c2a861e9fc86548baa659
Karl Hass <karlhass@Karls-MacBook-Pro.local>
2018-04-09 16:59:36 -0500

    homepage styles content

---
+++ app/design/frontend/Wagento/RAC/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: francisca
+ * Date: 3/5/18
+ * Time: 3:20 PM
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Wagento/RAC',
+    __DIR__
+);
